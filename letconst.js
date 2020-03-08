f82640d742406a3b189418121774eef34aadfb0a
sayeed hasan <sayeed.hasanbd10@gmail.com>
2020-03-08 14:33:09 -0700

    App:Updated App

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const hubby = "elias kanchon";
+// hubby = "omar Sunny";
+console.log(hubby);
+
+
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+numbers =["sunny", "omer", "salman"];
+
+const nayok = {name: "sakib", phone:45789};
+// console.log(numbers);
+
+let patinName = "rahim cahacha";
+patinName = "Fatema Khala";
+console.log(patinName);
+
+for (let i=0; i<10; i++ ){
+    sum = sum + i;
+}
+console.log(i);
